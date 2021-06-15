ae92aa02cb8cd36472b28e51adc75de1ca93bc82
Admin <admin@wb-mazhen.local>
2021-06-15 21:20:42 +0800

    add2

---
+++ 1-day01/12-ifv2.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 控制台输入一个成绩
+	// >= 90 =>A
+	// >= 80 => B
+	// >= 70 => C
+	// >= 60 => D
+	// 其他 => E
+	var  score int
+	fmt.Println("请输入成绩")
+	fmt.Scan(&score)
+	if score >=90{
+		fmt.Println("A")
+	}else if score >= 80{
+		fmt.Println("B")
+	}else if score >= 70  {
+		fmt.Println("C")
+	}else if score >=60 {
+		fmt.Println("D")
+	}else {
+		fmt.Println("F")
+
+	}
+
+}
---
+++ 1-day01/13_switch.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main ()  {
+	fmt.Println("老婆的想法")
+	fmt.Println("10个包子")
+	// 判断 是否有卖西瓜的 (控制台输入y)
+	// 有 买一个西瓜
+	var text  string
+	fmt.Println("有卖西瓜的吗")
+	fmt.Scan(&text)
+	switch text {
+	case "y":
+		fmt.Println("买一个西瓜")
+
+
+	}
+
+}
