0df096e927a72ee2369265441d5cf5ef5a92a24c
wb_mazhen <wb_mazhen@kuaishou.com>
2021-08-10 15:10:00 +0800

    [master]错误处理

---
+++ 3-day03/code/22-panic.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func test(p bool)  {
+	defer func() {
+		if err :=recover(); err != nil {
+			fmt.Println("出错了, 进行恢复", err)
+		}
+		
+	}()
+	if p {
+		panic("我是一个错误")
+	}
+
+	fmt.Println("running...")
+}
+
+
+func main() {
+	fmt.Println("11")
+	test(true)
+
+}
+
