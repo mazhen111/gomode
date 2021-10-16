7c4cb97b21a820562f7838bb45b90d8221bf8f57
wb_mazhen <wb_mazhen@kuaishou.com>
2021-10-16 17:04:03 +0800

    [master]接口11

--- day07/8-combind.go
+++ day07/8-combind.go
@@ -5,7 +5,7 @@ import "fmt"
 //组合接口
 //发送接口
 type Sender interface {
-	Sender(string) error
+	Sende(string) error
 }
 
 //代码的复用
@@ -14,12 +14,54 @@ type Sender interface {
 
 type Connection interface {
 	Sender
-	Open()
-	Recive() (string error)
+	Open() error
+	Recive() (string, error)
 	Close() error
+}
+type TcpConnection struct {
+}
+
+func (c *TcpConnection) Open() error {
+
+	return nil
+}
+
+func (c *TcpConnection) Recive() (string, error) {
+	return "", nil
+}
+
+func (c *TcpConnection) Sende(msg string) error {
+	return nil
+}
+func (c *TcpConnection) Close() error {
+	return nil
+}
+
+//结构体组合接口
+
+type Client struct {
+	Connection
+	C    Connection
+	Name string
 }
 
 func main() {
 	fmt.Println("11")
+	var conn Connection = new(TcpConnection)
+	fmt.Printf("%T, %#v\n", conn, conn)
+	conn.Open()
+	conn.Sende("xxxx")
+	conn.Recive()
+	conn.Close()
+	client := Client{new(TcpConnection), new(TcpConnection), "KK"}
+	client.Open()
+	client.Sende("xxx")
+	client.Recive()
+	client.Close()
+
+	client.C.Open()
+	client.C.Sende("xxxx")
+	client.C.Recive()
+	client.C.Close()
 
 }
